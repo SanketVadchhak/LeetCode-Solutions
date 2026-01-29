d6952610fc4612ca7e826a7bc067f2091bda6bfd
Sanket <sanket@Sankets-MacBook-Air.local>
2026-01-29 22:06:09 +0530

    first commit

---
+++ 1929/1929.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+
+class Solution {
+
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[2 * n];
+
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] nums = {1, 2, 1};
+
+        int[] result = sol.getConcatenation(nums);
+        System.out.println(Arrays.toString(result));
+    }
+}
+
+
+
+
+
+
+//.   LeetCode   .//
+/*  
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[2 * n];
+
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+
+        return ans;
+    }
+}
+*/
